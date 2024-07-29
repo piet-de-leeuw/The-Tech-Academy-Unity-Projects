8883748a6c12ec6cb925f4a3d6b4600ccebdf7db
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-07-29 14:41:05 +0200

    Adding Debug messages

---
+++ Magic cube/Assets/Cube.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welkom to the Console.");
+        Debug.Log("Press Left and rigt arrow keys to move.");
+        Debug.Log("Press Up arrow key to jump");
+        Debug.LogWarning("This is a warnning");
+        Debug.LogError("This should be an error");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
